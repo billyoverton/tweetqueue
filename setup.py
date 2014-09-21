9be531a4546c786b2f6b7dd2ee2dd9a30018698f
Billy Overton <overton.billy@gmail.com>
2014-09-21 05:01:22 -0400

    Added LICENSE and  updated setup.py information

--- setup.py
+++ setup.py
@@ -9,8 +9,22 @@ setup(
         'click>=3.3',
         'tweepy>=2.3.0',
     ],
+
+    author = "Billy Overton",
+    author_email = "billy@billyoverton.com",
+    description = "A command line tool for time-delaying your tweets.",
+    license = "MIT License",
+    keywords = "Twitter",
+    url = "https://github.com/billyoverton/tweetqueue",
+
     entry_points='''
         [console_scripts]
         tweetqueue=tweetqueue.__main__:tweetqueue
     ''',
+    classifiers= [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Utilities',
+        'Topic :: Communications'
+    ]
 )
